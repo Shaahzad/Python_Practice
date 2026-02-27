a4a5a520a0b3a1ee50949aa76fd4199c8731b20d
Shaahzad <shehzadmemon147@gmail.com>
2026-02-28 00:14:05 +0500

    chapter 1 practice complete

--- module.py
+++ module.py
@@ -1,4 +1,10 @@
-import pyjokes
+# import pyjokes
 
-joke = pyjokes.get_joke()
-print(joke)
+# joke = pyjokes.get_joke()
+# print(joke)
+
+import pyttsx3
+engine = pyttsx3.init()
+
+engine.say("Aman kese ho kha ho")
+engine.runAndWait()
