a1b7b0c940d341217ebef14b400bba969ceb6347
Shaahzad <shehzadmemon147@gmail.com>
2026-03-01 16:09:58 +0500

    learn variables and datatypes

---
+++ datatypes.py
@@ -0,0 +1,5 @@
+a = 1  # a is an integer
+b = 1.55  # b is a floating point number
+c = 'Shahzad' # c is a string
+d = False # d is a boolean
+e = None # e is a none type variable
---
+++ rules_variables.py
@@ -0,0 +1,8 @@
+a = 3
+aaaa10 = 5
+aman = 23
+shahzad = 45
+_shahzad = 45
+# @shahzad = 40 invalid due to @ symbol
+# 10shahzad = 20 variable name can not start with a digit only start with alphabet and underscores
+# no white space allowed in variables name 
