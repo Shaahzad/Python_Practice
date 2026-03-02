ed2fe2d524248f1f71c08473d9e919e8f5586e31
Shaahzad <shehzadmemon147@gmail.com>
2026-03-02 23:01:11 +0500

    Practice Set 2 Complete

---
+++ problem2.py
@@ -0,0 +1,29 @@
+# a = 1
+# b = 5
+
+# print(a + b)
+
+
+# a = 34
+
+# b = 5
+
+# print("Remainder when a is divided b is", a % b)
+
+# a = input("Enter value of a: ")
+# print(type(a))
+
+
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+
+# print("a is greater than b is ", a > b)
+
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+
+# print("The average of these two number is", (a+b)/2)
+
+a = int(input("Enter your number: "))
+print("The square of the number is", a**2)
+print("The square of the number is", a*a)
