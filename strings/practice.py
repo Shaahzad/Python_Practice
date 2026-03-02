7184261f1cf55316db523e1704b18a14df16c789
Shaahzad <shehzadmemon147@gmail.com>
2026-03-03 00:05:49 +0500

    Strings Chapter Complete

--- strings/practice.py
+++ strings/practice.py
@@ -2,6 +2,18 @@
 
 # print(f"Good After Noon {name}")
 
-letter = '''Dear <|Name|>, you are selected <|Date|> '''
+# letter = '''Dear <|Name|>, you are selected <|Date|> '''
 
-print(letter.replace('<|Name|>', 'Shahzad').replace('<|Date|>', '24 Sep 2026'))
+# print(letter.replace('<|Name|>', 'Shahzad').replace('<|Date|>', '24 Sep 2026'))
+
+name = "Shahzad and aman \n\t are friends"
+print(name)
+# print(name.find('  '))
+# print(name.find(' '))
+# print(name.find('am'))
+
+# print(name.replace(" ", "  "))
+
+
+
+# String are immutable which means that you cannot change them by running functions on them
