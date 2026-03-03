1a22f3948502ca32ad5ddbf435ea63fae60bef67
Shaahzad <shehzadmemon147@gmail.com>
2026-03-03 23:52:05 +0500

    Practice set Complete List and Tuple

---
+++ list/practice.py
@@ -0,0 +1,56 @@
+# fruit = []
+
+
+# f1 = input("Enter Fruit Name: ")
+# fruit.append(f1)
+# f2 = input("Enter Fruit Name: ")
+# fruit.append(f2)
+# f3 = input("Enter Fruit Name: ")
+# fruit.append(f3)
+# f4 = input("Enter Fruit Name: ")
+# fruit.append(f4)
+# f5 = input("Enter Fruit Name: ")
+# fruit.append(f5)
+# f6 = input("Enter Fruit Name: ")
+# fruit.append(f6)
+# f7 = input("Enter Fruit Name: ")
+# fruit.append(f7)
+# print(fruit)
+
+
+
+# marks = []
+
+# f1 = int(input("Enter marks here: "))
+# marks.append(f1)
+# f2 = int(input("Enter marks here: "))
+# marks.append(f2)
+# f3 = int(input("Enter marks here: "))
+# marks.append(f3)
+# f4 = int(input("Enter marks here: "))
+# marks.append(f4)
+# f5 = int(input("Enter marks here: "))
+# marks.append(f5)
+# f6 = int(input("Enter marks here: "))
+# marks.append(f6)
+
+# marks.sort()
+
+# print(marks)
+
+
+
+# a = (23, 43, "Shahzad")
+
+# a[2] = "Memon"
+
+
+# l = [23, 43, 56, 90]
+
+# print(sum(l))
+
+a = (2, 0, 0, 2, 0)
+
+no = a.count(0)
+
+print(no)
