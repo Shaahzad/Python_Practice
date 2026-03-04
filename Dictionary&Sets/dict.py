ea80905bf650c42ecfd5b980dee797d6bc269e8d
Shaahzad <shehzadmemon147@gmail.com>
2026-03-04 23:51:44 +0500

    learn dictionary and set

---
+++ Dictionary&Sets/dict.py
@@ -0,0 +1,15 @@
+marks = {
+    "Shahzad": 23,
+    "Aman": 90
+}
+
+# print(marks, type(marks))
+# print(marks["Shahzad"])
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Shahzad": 50})
+# print(marks)
+# print(marks.get("Asad"))
+print(marks.get("Shahzad2")) # print None
+print(marks["Shahzad2"]) # returns an error
---
+++ Dictionary&Sets/sets.py
@@ -0,0 +1,6 @@
+s = {1, 2, 50, 50, 32, 22, 52, 90, 3, 3, 2, "Harry"}
+s.add(56)
+print(s)
+s.remove(1)
+print(s)
+e = set() # Don't use s = {}  as it will create an empty dictionary
