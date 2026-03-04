f15aa3bc404069fff37a0fba885f67ecd07ea4ef
Shaahzad <shehzadmemon147@gmail.com>
2026-03-05 00:28:11 +0500

    Complete Dictionary & list

---
+++ Dictionary&Sets/practice.py
@@ -0,0 +1,70 @@
+# words = {
+#     "madad": "Help",
+#     "Kursi": "Chair",
+#     "billi": "Cat"
+# }
+
+# word = input("Enter the word you want meaning of: ")
+
+# print(words[word])
+
+
+# s = set()
+
+# n = input("Enter number: ")
+# s.add(int(n))
+# n = input("Enter number: ")
+# s.add(int(n))
+# n = input("Enter number: ")
+# s.add(int(n))
+# n = input("Enter number: ")
+# s.add(int(n))
+# n = input("Enter number: ")
+# s.add(int(n))
+# n = input("Enter number: ")
+# s.add(int(n))
+# n = input("Enter number: ")
+# s.add(int(n))
+# n = input("Enter number: ")
+# s.add(int(n))
+
+# print(s)
+
+
+# s = set()
+
+# s.add(18)
+# s.add('18')
+# print(s)
+
+# s = set()
+
+# s.add(18)
+# s.add(18.0)
+# s.add('18')
+# print(s)
+# print(len(s))
+
+# s = {}
+
+# print(type(s))
+
+d = {}
+
+Name = input("Enter friend Name: ")
+lang = input("Enter friend lang: ")
+d.update({Name: lang})
+
+Name = input("Enter friend Name: ")
+lang = input("Enter friend lang: ")
+d.update({Name: lang})
+
+Name = input("Enter friend Name: ")
+lang = input("Enter friend lang: ")
+d.update({Name: lang})
+
+Name = input("Enter friend Name: ")
+lang = input("Enter friend lang: ")
+d.update({Name: lang})
+
+print(d)
