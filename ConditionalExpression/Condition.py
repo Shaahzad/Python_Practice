9c2ab03223adeb75546b774c8fa3f0312fc9d475
Shaahzad <shehzadmemon147@gmail.com>
2026-03-05 23:52:50 +0500

    learn conditional statement

---
+++ ConditionalExpression/Condition.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your age: "))
+
+if(a>=18):
+    print("You are Above age")
+    print("Good For You")
+
+elif(a<0):
+    print("You are entering an invalid age")
+    
+else:
+    print("You are Below Age")   
