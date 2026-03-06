e388ec9a59e21494c59692f8c4bd0c2886609428
Shaahzad <shehzadmemon147@gmail.com>
2026-03-06 23:33:22 +0500

    Chapter 6 practice complete

---
+++ ConditionalExpression/practice.py
@@ -0,0 +1,92 @@
+# a1 = int(input("Enter Number 1: "))
+# a2 = int(input("Enter Number 2: "))
+# a3 = int(input("Enter Number 3: "))
+# a4 = int(input("Enter Number 4: "))
+
+
+# if(a1>a2 and a1>a3 and a1>a4):
+#     print("Greatest number is a1", a1)
+
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print("Greatest number is a2", a2)
+
+# elif(a3>a1 and a3>a2 and a3>a4):
+#     print("Greatest number is a3", a3)
+
+# elif(a4>a1 and a4>a2 and a4>a3):
+#     print("Greatest number is a4", a4)
+
+# marks1 = int(input("Enter Marks 1: "))
+# marks2 = int(input("Enter Marks 2: "))
+# marks3 = int(input("Enter Marks 3: "))
+
+# total_percentage = (100*(marks1 + marks2 + marks3)) / 300
+
+# if(total_percentage >= 40 and marks1>=33 and marks2>=33 and marks3>=33):
+#     print("You are Passed: ",  total_percentage)
+
+# else:
+#     print("You Failed: ", total_percentage)
+
+
+# p1 = "make alot of money"
+# p2 = "buy now"
+# p3 = "subscribe this"
+# p4 = "click this"
+
+# message = input("Enter your comment: ")
+
+# if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+#     print("This comment is a spam")
+
+# else:
+#     print("This comment is not a spam")
+
+
+# username = input("Enter username: ")
+
+# if(len(username)<10):
+#     print("Your username contain less than 10 characters")
+
+# else:
+#     print("All Is Well!")
+
+
+
+# l = ["Shahzad", "Memon", "Aman", "Sahibzada"]
+
+# name = input("Enter your name: ")
+
+# if(name in l):
+#     print('your name is in the list')
+
+# else:
+#     print("your name is not in the list")
+
+
+# marks = int(input("Enter your marks: "))
+
+
+# if(marks<=100 and marks>=90):
+#     grade = "Ex"
+# elif(marks<90 and marks>80):
+#     grade = "A"
+# elif(marks<80 and marks>70):
+#     grade = "B"
+# elif(marks<70 and marks>60):
+#     grade = "C"
+# elif(marks<60 and marks>50):
+#     grade = "D"
+# elif(marks<50):
+#     grade = "F"
+
+# print("Your Grade is: ", grade) 
+
+
+# post = input("Enter the post: ")
+
+# if("Shahzad".lower() in post.lower()): 
+#     print("This post is talking about Shahzad")
+
+# else:
+#     print("This post is not talking about Shahzad")
