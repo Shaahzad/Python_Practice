19674da6e5622acd60a7d088c055c2c7adb47d9f
Shaahzad <shehzadmemon147@gmail.com>
2026-03-07 07:33:29 +0500

    learn loop in python

---
+++ loop/whileloop.py
@@ -0,0 +1,47 @@
+# i = 1
+
+# while(i<6):
+#     print(i)
+#     i+=1
+
+# l = ["Shahzad", 23, True, "Aman"]
+
+# i = 0
+
+# while(i<len(l)):
+#     print(l[i])
+#     i += 1
+
+
+
+# for loop with strings
+# name = "Shahzad"
+
+# for i in name:
+#     print(i)
+
+# for loop with list
+# l = [1, 2, 3, 100]
+
+# for i in l:
+#     print(i)
+
+
+# for loop with tuple
+# t = (1, 2, 50, 900)
+
+# for i in t:
+#     print(i)
+
+
+# for i in range(0, 100, 4):
+#     print(i)
+
+
+l = [1, 4, 78]
+
+for i in l:
+    print(i)
+
+else:
+    print("Done")
