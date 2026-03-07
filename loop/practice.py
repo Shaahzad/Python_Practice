d39c7fc03005c2b8109b0fbd3ae9ee81b9499edf
Shaahzad <shehzadmemon147@gmail.com>
2026-03-08 00:43:16 +0500

    practice loop in python complete

--- loop/practice.py
+++ loop/practice.py
@@ -48,4 +48,30 @@
 
 # print(f"The Factorial Of {n} is {product}")
 
+# n = int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     print(" "* (n-i), end="")
+#     print("*"* (2*i-1), end="")
+#     print("")
 
+# n = int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     print("*"* i, end="")
+#     print("")
+
+
+# n = int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     if(i==1 or i==n):
+#         print("*"* n, end="")
+#     else:
+#         print("*", end="")
+#         print(" "* (n-2), end="")
+#         print("*", end="")
+#     print(" ")
+
+
+# n = int(input("Enter a number: "))
+
+# for i in range(1, 11):
+#     print(f"{n} X {11 - i} {n * (11 - i)}")
