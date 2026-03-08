7da9673db25874e3398d7c4914a7958ca256f2ba
Shaahzad <shehzadmemon147@gmail.com>
2026-03-08 16:47:05 +0500

    learn practice & Recursion

---
+++ Function&Recursions/Function.py
@@ -0,0 +1,35 @@
+# def avg():
+#     a = int(input("Enter Your Number: "))
+#     b = int(input("Enter Your Number: "))
+#     c = int(input("Enter Your Number: "))
+
+#     average = (a + b + c) /3
+#     print(average)
+
+# avg()
+
+
+# def GoodDay(name):
+#     print("Good Day " + name)
+
+# GoodDay("Shahzad")
+
+
+# def avg():
+#     a = int(input("Enter Your Number: "))
+#     b = int(input("Enter Your Number: "))
+#     c = int(input("Enter Your Number: "))
+
+#     average = (a + b + c) /3
+#     return average
+
+# a = avg()
+# print(a)
+
+
+
+# def GoodDay(name, ending="Thank You"):
+#     print(f"Good Day {name}")
+#     print(ending)
+
+# GoodDay("Shahzad")
