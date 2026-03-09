d3fb397796722af314d4cf9b868733a622012a04
Shaahzad <shehzadmemon147@gmail.com>
2026-03-09 23:52:34 +0500

    Complete project 1 with python

---
+++ Project1/main.py
@@ -0,0 +1,32 @@
+import random
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDic = {"s": 1, "w": -1, "g": 0}
+reverseDic = {1: "Snake", -1: "Water", 0: "Gun"}
+you = youDic[youstr]
+
+print(f"You Chose {reverseDic[you]}\nComputer Chose {reverseDic[computer]}")
+if(computer == you):
+    print('its a draw')
+
+else:
+    if(computer == -1 and you == 1): 
+        print("You Win!")
+
+    elif(computer == -1 and you == 0):
+        print("You Lose!")
+
+    elif(computer == 1 and you == -1):
+        print("You Lose!")
+
+    elif(computer == 1 and you == 0):
+        print("You Win!")
+
+    elif(computer == 0 and you == -1):
+        print("You Win!")
+
+    elif(computer == 0 and you == 1):
+        print("You lose!")
+
+    else:
+        print("Something Went Wrong")
