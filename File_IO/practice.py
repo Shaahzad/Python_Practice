4fbd83519d19b9a40f1aa6cd11a29218bfe54d3a
Shaahzad <shehzadmemon147@gmail.com>
2026-03-10 23:51:53 +0500

    Complete Practice File_IO

--- File_IO/practice.py
+++ File_IO/practice.py
@@ -44,3 +44,78 @@
 
 # for i in range(2, 21):
 #     generateTable(i)
+
+
+# word = "Donkey"
+
+# with open("file1.txt", "r") as f:
+#     content = f.read()
+
+# contentNew = content.replace(word, "####")
+
+# with open("file1.txt", "w") as f:
+#     f.write(contentNew)
+
+
+# words = {"Donkey", "bad", "ganda"}
+
+# with open("file1.txt", "r") as f:
+#     content = f.read()
+
+# for word in words:
+#     content = content.replace(word, "#" * len(word))
+
+# with open("file1.txt", "w") as f:
+#     f.write(content)
+
+
+# with open("log.txt") as f:
+#     content = f.read()
+
+# if("Python" in content):
+#     print("Yes python is present")
+# else:
+#     print("No python is not present")
+
+
+# with open("log.txt") as f:
+#     lines = f.readlines()
+
+# lineno = 1
+# for line in lines:
+#     if("python" in line):
+#         print(f"yes python is present in line no: {lineno}")
+#         break
+#     lineno += 1
+
+# else:
+#     print("python is not present")
+
+
+# with open("this.txt") as f:
+#     content = f.read()
+
+# with open("this_copy.txt", "w") as f:
+#     f.write(content)
+
+# with open("this.txt") as f:
+#     content1 = f.read()
+
+# with open("this_copy.txt") as f:
+#     content2 = f.read()
+
+# if(content1 == content2):
+#     print("Yes content Same")
+# else:
+#     print("No Content Not Same")
+
+# with open("this_copy.txt", "w") as f:
+#     f.write("")
+
+
+
+# with open("file.txt") as f:
+#     content = f.read()
+
+# with open("rename.txt", "w") as f:
+#     f.write(content)
