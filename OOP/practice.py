bc2721464ccf4c8fa1fe2cf10f34b6339ef21840
Shaahzad <shehzadmemon147@gmail.com>
2026-03-12 23:13:58 +0500

    practice oop

---
+++ OOP/practice.py
@@ -0,0 +1,29 @@
+# class Programmer:
+#     company = "Microsoft"
+#     def __init__(self, name, salary, pin):
+#         self.name = name
+#         self.salary = salary
+#         self.pin = pin
+
+# p = Programmer("Shahzad", 1200000, 2300)
+
+# print(p.name, p.company, p.pin, p.salary)
+
+
+class Calculator:
+    def __init__(self, n):
+        self.n = n
+    
+    def square(self):
+        print(f"The square is {self.n * self.n}")
+    
+    def cube(self):
+        print(f"The cube is {self.n * self.n * self.n}")
+    
+    def squareroot(self):
+        print(f"The squareroot is {self.n ** 1/2}")
+
+a = Calculator(4)
+a.square()
+a.cube()
+a.squareroot()
