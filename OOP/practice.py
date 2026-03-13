81a1519ee4411c73d5f2ad8607e8cec68074cbbf
Shaahzad <shehzadmemon147@gmail.com>
2026-03-14 00:27:30 +0500

    Complete OOP Practice

--- OOP/practice.py
+++ OOP/practice.py
@@ -22,8 +22,39 @@
     
 #     def squareroot(self):
 #         print(f"The squareroot is {self.n ** 1/2}")
+    
+#     @staticmethod
+#     def Cool():
+#         print("Hello Cool")
 
 # a = Calculator(4)
+# a.Cool()
 # a.square()
 # a.cube()
 # a.squareroot()
+
+# class Demo:
+#     a = 4
+
+# o = Demo()
+# print(o.a) # print class attribute because instance attribute is not present  
+# o.a = 0 # instance attribute is set
+# print(o.a)  # print instance attribute because instance attribute is present
+# print(Demo.a) # print the class attribute
+
+# from random import randint
+# class Train:
+
+#     def book(self, trainNo, fro, to):
+#         print(f"Ticket is booked in train no: {trainNo} from {fro} to {to}")
+
+#     def getStatus(self, trainNo):
+#         print(f"Train no: {trainNo} is running on time")
+
+#     def getFare(self, trainNo, fro, to):
+#         print(f"Ticket fare in train no: {trainNo} from {fro} to {to} is {randint(22, 55)}")
+
+
+# t = Train()
+# t.book(23, "Karachi", "Lahore")
+# t.getFare(23, "Karachi", "Lahore")
