9b321d0cbf7f0320e49bce13673e8896440808d1
Shaahzad <shehzadmemon147@gmail.com>
2026-03-13 23:48:17 +0500

    practice oop

--- OOP/practice.py
+++ OOP/practice.py
@@ -10,20 +10,20 @@
 # print(p.name, p.company, p.pin, p.salary)
 
 
-class Calculator:
-    def __init__(self, n):
-        self.n = n
+# class Calculator:
+#     def __init__(self, n):
+#         self.n = n
     
-    def square(self):
-        print(f"The square is {self.n * self.n}")
+#     def square(self):
+#         print(f"The square is {self.n * self.n}")
     
-    def cube(self):
-        print(f"The cube is {self.n * self.n * self.n}")
+#     def cube(self):
+#         print(f"The cube is {self.n * self.n * self.n}")
     
-    def squareroot(self):
-        print(f"The squareroot is {self.n ** 1/2}")
+#     def squareroot(self):
+#         print(f"The squareroot is {self.n ** 1/2}")
 
-a = Calculator(4)
-a.square()
-a.cube()
-a.squareroot()
+# a = Calculator(4)
+# a.square()
+# a.cube()
+# a.squareroot()
