6aa440137fed0e876cf551ed46a4eb106ddb131e
Shaahzad <shehzadmemon147@gmail.com>
2026-03-18 23:30:24 +0500

    learn chapter 13

---
+++ Chapter13/practice.py
@@ -0,0 +1,33 @@
+# name = input("Enter name: ")
+# marks = int(input("Enter marks: "))
+# phone = int(input("Enter phone: "))
+
+# s = "The name of the student is {} and marks is {} and phone number is {}".format(name, marks, phone)
+# print(s)
+
+
+# table = [str(5 * i) for i in range(1, 11)]
+
+# s = "\n".join(table)
+# print(s)
+
+
+# def divisible5(n):
+#     if(n%5 == 0):
+#         return True
+#     return False
+
+# a = [34, 25, 30, 50, 100, 123, 125]
+
+# f = list(filter(divisible5, a))
+# print(f)
+
+# from functools import reduce
+# l = [34, 25, 30, 50, 100, 123, 125]
+
+# def greater(a,b):
+#     if(a>b):
+#         return a
+#     return b
+
+# print(reduce(greater, l))
