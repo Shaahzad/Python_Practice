8c89a61f7d2d27889b824f7c9388abb7c25f4a38
Shaahzad <shehzadmemon147@gmail.com>
2026-03-18 23:36:14 +0500

    learn chapter 13 Complete

--- Chapter13/practice.py
+++ Chapter13/practice.py
@@ -31,3 +31,18 @@
 #     return b
 
 # print(reduce(greater, l))
+
+
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+app.run()
